ccd19397d8168f8e92db84988b5c441f9710f34a
Marco Ocanto <mocanto@nube.gob.ve>
2024-12-13 09:47:30 -0400

    routes pruebas

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,34 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
 });
+
+// Route::get('/', function () {
+//     return 'Bienvenido';
+// });
+
+
+// Route::get('blog', function () {
+//     return 'Estudios Nuevos';
+// });
+
+// Route::get('blog/{slug}', function ($slug) {
+//     return  $slug;
+// });
+
+// Route::get('buscar', function (Request $request) {
+//     return $request->all();
+// });
+
+Route::get('blog', function () {
+    $post=[
+        ['id'=>1,'title'=>'PHP', 'slug'=>'laravel'],
+        ['id'=>2,'title'=>'Python', 'slug'=>'django']
+
+    ];
+    return view('blog',['post'=>$post]);
+ });
+
+
+ 
