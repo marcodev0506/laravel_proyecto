ad08de4562da39d2687752522b51efd321a527a1
Marco Ocanto <mocanto@nube.gob.ve>
2025-01-21 16:22:42 -0400

    Factory y Seeder

--- database/migrations/2025_01_08_201806_create_post_table.php
+++ database/migrations/2025_01_08_201806_create_post_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('post', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
+            $table->string('slug');    
+            $table->text('body');
             $table->timestamps();
         });
     }
